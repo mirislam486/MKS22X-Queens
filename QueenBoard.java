759a49a38dd5a8b348c06451ff22f2c16ad53c4a
mirislam486 <34451472+mirislam486@users.noreply.github.com>
2019-02-07 23:20:33 -0500

    Add files via upload

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,6 +3,11 @@ public class QueenBoard{
 
   public QueenBoard(int size){
     int[][] board = new int[size][size];
+    for(int i = 0; i < size; i++){
+      for(int x = 0; x < size; x++){
+        board[i][x] = 0;
+      }
+    }
   }
 
   private boolean addQueen(int r, int c){
